f57a3ddb3bfe3b26278d83f8a367a4b645d99174
zeeyeeteecar <101447550+zeeyeeteecar@users.noreply.github.com>
2024-09-05 14:53:44 -0700

    4

--- src/app/acm2024/page.tsx
+++ src/app/acm2024/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { revalidatePath } from "next/cache";
 import SideNavbar from "../components/SideNavbar";
 import Acm2024_Block from "./components/Acm2024_Block";
+import Image from "next/image";
 
 export default function page() {
   revalidatePath("/acm2024");
@@ -12,8 +13,16 @@ export default function page() {
       <div className="w-full block border-0">
         <SideNavbar />
       </div>
-      <div className="w-full h-[1000px] flex flex-grow ">
-        <div className="w-[300px] h-[300px] border-2"></div>
+      <div className="w-full h-[1000px] flex flex-grow flex-row ">
+        <div className=" w-[300px] flex flex-col bg-white rounded-2xl p-0 gap-4 border-0">
+          <Image
+            src="/ACM2024.jpg"
+            className="w-[300px]"
+            width={300}
+            height={300}
+            alt={"tea"}
+          ></Image>
+        </div>
         <Acm2024_Block />
       </div>
     </div>
